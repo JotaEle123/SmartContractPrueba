17ea20e9d1559b785157ce730aced81e86ba24ff
Jose Luis Soto <123tuyoes@gmail.com>
2022-02-23 23:30:21 +0100

    config

--- truffle.js
+++ truffle.js
@@ -1,48 +1,44 @@
 require('dotenv').config()
-const HDWalletProvider = require('@truffle/hdwallet-provider');
+import HDWalletProvider from '@truffle/hdwallet-provider';
 
-const fs = require('fs');
+import fs from 'fs';
 
-module.exports = {
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*",
+export const networks = {
+  development: {
+    host: "127.0.0.1",
+    port: 8545,
+    network_id: "*",
+  },
+
+  ropsten: {
+    provider: function () {
+      return new HDWalletProvider(process.env.PRIVATE_KEY, process.env.ROPSTEN_RPC_URL);
     },
-
-    ropsten: {
-      provider: function () {
-        return new HDWalletProvider(process.env.PRIVATE_KEY, process.env.ROPSTEN_RPC_URL);
-      },
-      network_id: 3,
-      skipDryRun: true,
-    }
-
-/*
-     rinkeby: {
-      provider: function () {
-        return new HDWalletProvider(process.env.PRIVATE_KEY, process.env.RINKEBY_RPC_URL);
-      },
-      network_id: 4,
-      skipDryRun: true,
-    },
-    mainnet: {
-      provider: function () {
-        return new HDWalletProvider(process.env.PRIVATE_KEY, process.env.MAINNET_RPC_URL);
-      },
-      network_id: 1,
-      skipDryRun: true,
-    }  */
-  },
-  mocha: {
-  },
-  compilers: {
-    solc: {
-      version: "0.8.9",
-    }
-  },
-  db: {
-    enabled: false
+    network_id: 3,
+    skipDryRun: true,
   }
-}
+  /*
+       rinkeby: {
+        provider: function () {
+          return new HDWalletProvider(process.env.PRIVATE_KEY, process.env.RINKEBY_RPC_URL);
+        },
+        network_id: 4,
+        skipDryRun: true,
+      },
+      mainnet: {
+        provider: function () {
+          return new HDWalletProvider(process.env.PRIVATE_KEY, process.env.MAINNET_RPC_URL);
+        },
+        network_id: 1,
+        skipDryRun: true,
+      }  */
+};
+export const mocha = {};
+export const compilers = {
+  solc: {
+    version: "0.8.9",
+  }
+};
+export const db = {
+  enabled: false
+};
